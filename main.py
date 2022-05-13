c9b9e221fc1e9fee56a9c33de92ff4857b1733f9
caedawen <d.b.vance@hotmail.com>
2022-05-13 21:04:22 +0200

    Weather app finalized

---
+++ main.py
@@ -0,0 +1,14 @@
+from gui import *
+import sys
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
